9c954ae4314446799081f6c525645ce12f6f35ad
dogrunjp <oec@dogrun.jp>
2023-07-30 15:23:58 +0900

    Created data loading module (in progress)

---
+++ docs/read_from_txt.py
@@ -0,0 +1,20 @@
+# encoding: utf-8
+import os
+import sys
+import csv
+
+
+def main():
+    """_summary_
+    0. The source is a tab-delimited file in which nodes, edges, 
+    and anchors are described block by block, and the comment line 
+    on the first line of each block begins with nodes, edges, and anchors.
+    1. Read tab-delimited file describing nodes, edges, and anchors.
+    2. Parses rows for each block and converts to a list or dict.
+    3. Returns a list of dicts for each block.
+    """
+
+
+if __name__ == '__main__':
+    main()
+    
